e1bf30fc9f4a5b80f2cc85bfb587c9fc399211ee
Tobia Gasparoni <tr.gasparoni@uniandes.edu.co>
2021-03-14 22:31:54 -0500

    Parcial finalizado

--- index.js
+++ index.js
@@ -1,243 +1,290 @@
 const listado = "data.json";
 
-var numItems = 0;
+const cats = ["Burgers", "Tacos", "Salads", "Desserts", "Drinks and Sides"];
 
-var cats = ["Burguers", "Tacos", "Salads", "Desserts", "Drinks & Sides"]
+let cur_cat = null;
+
+let cart = [];
 
 function readJson (url) {
     return new Promise((resolve, reject) => {
         let req = new XMLHttpRequest();
         req.open("GET", url);
         req.onload = function() {
-            (req.status===200) ? (resolve(JSON.parse(req.responseText))) : (reject(req.statusText))
+            (req.status===200) ? (resolve(JSON.parse(req.responseText))) : (reject(req.statusText));
         };
         req.send();
     });
 }
 
-for(let cat = 0; cat < cats.length; cat++) {
-    let item = document.getElementById(cats[cat]);
+for(let i = 0; i < cats.length; i++) {
+    let item = document.getElementById(cats[i]);
     item.addEventListener("click", () => {
-        let list = document.getElementById("list");
-        list.innerHTML = "";
-        renderCategory(cats[cat]);
+        menuClick(cats[i]);
     });
 }
 
-readJson(listado).then((listadoArr) => {
-    console.log(listadoArr);
-});
+function menuClick(cat) {
+    if(cat != cur_cat) {
+        cur_cat = cat;
+        document.getElementById("list").innerHTML = "";
+        renderCategory(cat);
+    }
+}
 
 function renderCategory(category) {
+    document.getElementById("table").style.display = "none";
     readJson(listado).then((listadoArr) => {
-        console.log(listadoArr);
-        for(let cat = 0; cat < listadoArr.length; cat++) {
-            if(listadoArr[cat].name === category) {
-        
-                let card = document.createElement("div");
-                card.style = "width: 18rem;";
-                card.className = "card";
+        for(let i = 0; i < listadoArr.length; i++) {
+            if(listadoArr[i].name === category) {
+                let title = document.getElementById("cat-title");
+                title.style.display = "block";
+                title.innerHTML = "<hr/>" + listadoArr[i].name + "<hr/>";
 
-                let img = document.createElement("img");
-                img.className = "card-img-top";
-                img.src = listadoArr[cat].image;
-                img.alt = "Food item";
-                card.appendChild(img);
+                let row = document.createElement("div");
+                row.className = "row";
 
-                let cardBody = document.createElement("div");
-                cardBody.className = "card-body";
+                let products = listadoArr[i].products;
+                for(let j = 0; j < products.length; j++) {
 
-                let h5 = document.createElement("h5");
-                h5.className = "card-title";
-                h5.appendChild(document.createTextNode(listadoArr[cat].name));
-                cardBody.appendChild(h5);
+                    let cardContainer = document.createElement("div");
+                    cardContainer.className = "col-3";
+                    cardContainer.style.padding = "3px";
 
-                let desc = document.createElement("p");
-                desc.className = "card-text";
-                desc.appendChild(document.createTextNode(listadoArr[cat].description));
-                cardBody.appendChild(desc);
+                    let card = document.createElement("div");
+                    card.className = "card";
 
-                let strong = document.createElement("strong");
-                strong.appendChild(document.createTextNode(listadoArr[cat].price));
-                cardBody.appendChild(strong);
+                    let img = document.createElement("img");
+                    img.className = "card-img-top";
+                    img.style.width = "100%";
+                    img.style.height = "15rem";
+                    img.alt = products[j].name;
+                    img.src = products[j].image;
+                    img.style.objectFit = "cover";
+                    img.alt = "Food item";
+                    card.appendChild(img);
 
-                card.appendChild(cardBody);
+                    let cardBody = document.createElement("div");
+                    cardBody.className = "card-body";
 
-                let button = document.createElement("button");
-                button.appendChild(document.createTextNode("Add to cart"));
-                button.addEventListener("click", () => {
-                    numItems += 1;
-                    let itemsElem = document.getElementById("numItems");
-                    itemsElem.innerHTML(numItems);
-                });
+                    let h5 = document.createElement("h5");
+                    h5.className = "card-title";
+                    h5.appendChild(document.createTextNode(products[j].name));
+                    cardBody.appendChild(h5);
 
-                card.appendChild(button);
+                    let desc = document.createElement("p");
+                    desc.className = "card-text";
+                    desc.appendChild(document.createTextNode(products[j].description));
+                    cardBody.appendChild(desc);
 
-                document.getElementById("list").appendChild(card);
+                    let strong = document.createElement("strong");
+                    strong.appendChild(document.createTextNode("$" + products[j].price));
+                    cardBody.appendChild(strong);
+
+                    card.appendChild(cardBody);
+
+                    let button = document.createElement("button");
+                    button.className = "btn btn-dark";
+                    button.appendChild(document.createTextNode("Add to cart"));
+                    button.addEventListener("click", () => {
+                        addToCart(products[j]);
+                        updateNumItems();
+                    });
+
+                    card.appendChild(button);
+
+                    cardContainer.appendChild(card);
+                    row.appendChild(cardContainer);
+                }
+                document.getElementById("list").appendChild(row);
             }
         }
     });
 }
 
-/*
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="oneburger.png" alt="Burger item">
-    <div class="card-body">
-        <h5 id="burg-title" class="card-title">Card title</h5>
-        <p id="burg-desc" class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <strong id="burg-price">Price</strong>
-    </div>
-    <button>Add to cart</button>
-</div>
-*/
-
-/*
-function createEntry() {
-    let entry = {
-        "last_name": document.getElementById("last-name").value,
-        "first_name": document.getElementById("first-name").value,
-        "email": document.getElementById("email").value,
-        "photo": "http://dummyimage.com/155x119.png/ff4444/ffffff"
-    };
-    people.push(entry);
-    if(last_name_asc !== NEUTRAL)
-        order(LAST_NAME, last_name_asc === ASC);
-    if(first_name_asc !== NEUTRAL)
-        order(FIRST_NAME, first_name_asc === ASC);
-    if(email_asc !== NEUTRAL)
-        order(FIRST_NAME, email_asc === ASC);
-    display();
-}
-
-// Function to add event listener to every heading except photo!
-// Also loads event listener to submit button
-function loadEventListeners() {
+function addToCart(product) {
+    let prodInList = null;
     
-    let headLastName = document.createElement("td");
-    headLastName.appendChild(document.createTextNode("Last Name"));
-    headLastName.addEventListener("click", function () {
-        if(last_name_asc <= 0) {
-            order(LAST_NAME, true);
-            last_name_asc = ASC;
-        } else{
-            order(LAST_NAME, false);
-            last_name_asc = DESC;
+    for(let i = 0; i < cart.length; i++) {
+        if(cart[i].name === product.name) {
+            prodInList = cart[i];
+            prodInList.quantity += 1;
         }
-        setToNeutral(LAST_NAME);
-    });
+    }
 
-    let headFirstName = document.createElement("td");
-    headFirstName.appendChild(document.createTextNode("First Name"));
-    headFirstName.addEventListener("click", function () {
-        if(first_name_asc <= 0) {
-            order(FIRST_NAME, true);
-            first_name_asc = ASC;
-        } else if (first_name_asc > 0) {
-            order(FIRST_NAME, false);
-            first_name_asc = DESC;
-        }
-        setToNeutral(FIRST_NAME);
-    });
-
-    let headEmail = document.createElement("td");
-    headEmail.appendChild(document.createTextNode("Email"));
-    headEmail.addEventListener("click", function () {
-        if(email_asc <= 0) {
-            order(EMAIL, true);
-            email_asc = ASC;
-        } else if (email_asc > 0) {
-            order(EMAIL, false);
-            email_asc = DESC;
-        }
-        setToNeutral(EMAIL);
-    });
-
-    let headPhoto = document.createElement("td");
-    headPhoto.appendChild(document.createTextNode("Photo"));
-
-    let thead = document.getElementById("thead");
-    thead.appendChild(headLastName);
-    thead.appendChild(headFirstName);
-    thead.appendChild(headEmail);
-    thead.appendChild(headPhoto);
-
-    const formCreate = document.getElementById('create');
-    formCreate.addEventListener('click', createEntry);
+    if(prodInList == null) {
+        product.quantity = 1;
+        cart.push(product);
+    }
 }
 
-function setToNeutral(except) {
-    if(except !== LAST_NAME) {last_name_asc = NEUTRAL;}
-    if(except !== FIRST_NAME) {first_name_asc = NEUTRAL;}
-    if(except !== EMAIL) {email_asc = NEUTRAL;}
+function removeOneFromCart(prodName) {
+    let isOne = false;
+
+    for(let i = 0; i < cart.length; i++) {
+        prodInList = cart[i];
+        if(prodInList.name === prodName) {
+            if(prodInList.quantity === 1) {
+                isOne = true;
+            }
+            else {
+                prodInList.quantity -= 1;
+            }
+        }
+    }
+
+    if(isOne) {
+        cart = cart.filter(function(elem) { return elem.name != prodName; }); 
+    }
 }
 
-function display() {
+function updateNumItems() {
+    let numItems = 0;
+
+    for(let i = 0; i < cart.length; i++) {
+        numItems += cart[i].quantity;
+    }
+
+    let itemsElem = document.getElementById("numItems");
+    itemsElem.innerHTML = numItems;
+}
+
+function updateTotalPrice() {
+    let totPrice = 0;
+
+    for(let i = 0; i < cart.length; i++) {
+        totPrice += cart[i].quantity * cart[i].price;
+    }
+
+    let totPriceElem = document.getElementById("tot");
+    totPriceElem.innerHTML = Math.round(totPrice * 100) / 100;
+}
+
+document.getElementById("cart").addEventListener("click", () => {
+    displayCart();
+});
+
+function displayCart() {
+    cur_cat = null;
+
+    document.getElementById("table").style.display = "block";
+    document.getElementById("list").innerHTML = "";
+    
+    let title = document.getElementById("cat-title");
+    title.style.display = "block";
+    title.innerHTML = "<hr/>" + "Order detail" + "<hr/>";
+
     let tbody = document.getElementById("tbody");
+
     while(tbody.firstChild) {
         tbody.removeChild(tbody.firstChild);
-    };
+    }
 
-    for(let i = 0; i < people.length; i++) {
-        let person = people[i];
+    for(let i = 0; i < cart.length; i++) {
+        let product = cart[i];
         let row = document.createElement("tr");
 
-        let tdLastName = document.createElement("td");
-        tdLastName.appendChild(document.createTextNode(person.last_name));
+        let tdItem = document.createElement("th");
+        tdItem.scope = "row";
+        tdItem.appendChild(document.createTextNode(i+1));
         
-        let tdFirstName = document.createElement("td");
-        tdFirstName.appendChild(document.createTextNode(person.first_name));
+        let tdQuantity = document.createElement("td");
+        tdQuantity.appendChild(document.createTextNode(product.quantity));
         
-        let tdEmail = document.createElement("td");
-        tdEmail.appendChild(document.createTextNode(person.email));
+        let tdDescription = document.createElement("td");
+        tdDescription.appendChild(document.createTextNode(product.name));
         
-        let tdPhoto = document.createElement("td");
-        let imgPhoto = document.createElement("img");
-        imgPhoto.setAttribute("src", person.photo);
-        tdPhoto.appendChild(imgPhoto);
+        let tdUnitPrice = document.createElement("td");
+        tdUnitPrice.appendChild(document.createTextNode(product.price));
 
-        row.appendChild(tdLastName);
-        row.appendChild(tdFirstName);
-        row.appendChild(tdEmail);
-        row.appendChild(tdPhoto);
+        let tdAmount = document.createElement("td");
+        let amount = product.price * product.quantity;
+        tdAmount.appendChild(document.createTextNode(Math.round(amount * 100) / 100));
+
+        let tdBtns = document.createElement("td");
+
+        let butPlus = document.createElement("button");
+        butPlus.appendChild(document.createTextNode("+"));
+        butPlus.className = "btn btn-secondary margin-right";
+        butPlus.addEventListener("click", () => {
+            let elementToBeAdded = Object.assign({}, product);
+            delete elementToBeAdded["quantity"];
+            addToCart(elementToBeAdded);
+            updateNumItems();
+            displayCart();
+        });
+
+        let butMinus = document.createElement("button");
+        butMinus.appendChild(document.createTextNode("-"));
+        butMinus.className = "btn btn-secondary";
+        butMinus.addEventListener("click", () => {
+            removeOneFromCart(product.name);
+            updateNumItems();
+            displayCart();
+        });
+
+        tdBtns.appendChild(butPlus);
+        tdBtns.appendChild(butMinus);
+
+        row.appendChild(tdItem);
+        row.appendChild(tdQuantity);
+        row.appendChild(tdDescription);
+        row.appendChild(tdUnitPrice);
+        row.appendChild(tdAmount);
+        row.appendChild(tdBtns);
         
         tbody.appendChild(row);
     }
+    updateTotalPrice();
 }
 
-function compare(x, y, asc) {
-    if(asc) {
-        if (x < y) {return -1;}
-        if (x > y) {return 1;}
-    } else {
-        if (x < y) {return 1;}
-        if (x > y) {return -1;}
+document.getElementById("close-alert").addEventListener("click", () => {
+    let cancel = document.getElementById("cancel-background");
+    cancel.classList.remove("d-flex");
+    cancel.classList.add("d-none");
+});
+
+document.getElementById("no-cancel").addEventListener("click", () => {
+    let cancel = document.getElementById("cancel-background");
+    cancel.classList.remove("d-flex");
+    cancel.classList.add("d-none");
+});
+
+document.getElementById("yes-cancel").addEventListener("click", () => {
+    cart = [];
+    displayCart();
+    updateNumItems();
+    
+    let cancel = document.getElementById("cancel-background");
+    cancel.classList.remove("d-flex");
+    cancel.classList.add("d-none");
+});
+
+document.getElementById("btn-cancel").addEventListener("click", () => {
+    let cancel = document.getElementById("cancel-background");
+    cancel.classList.remove("d-none");
+    cancel.classList.add("d-flex");
+});
+
+document.getElementById("btn-confirm").addEventListener("click", () => {
+    order = [];
+
+    for(let i = 0; i < cart.length; i++) {
+        order.push({
+            "item": i+1,
+            "quantity": cart[i].quantity,
+            "description": cart[i].name,
+            "unitPrice": cart[i].price
+        });
     }
-    return 0;
-}
 
-function order(param, asc) {
-    if(param === LAST_NAME) {
-        people.sort(function(a, b){
-            var x = a.last_name;
-            var y = b.last_name;
-            return compare(x, y, asc);
-        });
-    } else if((param === FIRST_NAME)) {
-        people.sort(function(a, b){
-            var x = a.first_name;
-            var y = b.first_name;
-            return compare(x, y, asc);
-        });
-    } else if((param === EMAIL)) {
-        people.sort(function(a, b){
-            var x = a.email;
-            var y = b.email;
-            return compare(x, y, asc);
-        });
-    }
-    display();
-}
+    console.log(order);
+});
 
-loadEventListeners();
-display();
-*/
+cur_cat = "Burgers";
+renderCategory("Burgers");
+document.getElementById("table").style.display = "none";
+
+let cancel = document.getElementById("cancel-background");
+cancel.classList.remove("d-flex");
+cancel.classList.add("d-none");
